390a8f51cc940de3d6d87285420a1cb2073e56a7
Frederico Sampaio <fredimpv@gmail.com>
2018-06-23 21:13:31 -0300

    Update main.js

--- adm/experimentos/js/main.js
+++ adm/experimentos/js/main.js
@@ -1,9 +1,9 @@
 var config = {
-    apiKey: "AIzaSyDJx4SzxdKmMO-VwUac8jGyZZdAZXmzCPQ",
-    authDomain: "experimento-parte3.firebaseapp.com",
-    databaseURL: "https://experimento-parte3.firebaseio.com",
-    projectId: "experimento-parte3",
-    storageBucket: "",
-    messagingSenderId: "941074159016"
+    apiKey: "#",
+    authDomain: "#",
+    databaseURL: "#",
+    projectId: "#",
+    storageBucket: "#",
+    messagingSenderId: "#"
   };
   firebase.initializeApp(config);
